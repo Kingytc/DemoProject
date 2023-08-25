377f51203beb45cca7f69ab3f1fabff5211ec188
shiva <shivaranjannagathan.n@gmail.com>
2023-08-25 12:06:36 +0530

    doc

---
+++ demo/src/main/java/practices/PPPPPPPPP.java
@@ -0,0 +1,33 @@
+package practices;
+
+public class PPPPPPPPP {
+	
+		public static void main(String[] args) {
+			String name = "Demosystem";
+			
+			if (name.equalsIgnoreCase("demoSYSTEM")) {
+				System.out.println("True");
+				
+			} else {
+				System.out.println("False");
+			}
+		}
+
+}
+
+
+class demo {
+	public static void main(String[] args) {
+		int n=5;
+		int i,j;
+		
+		for(i=0;i<=5;i++)
+		{
+			for(j=1;j<=4;j++)
+			{
+				System.out.println("*");
+			}
+		}
+		
+	}
+}
